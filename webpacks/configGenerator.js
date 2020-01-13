e013dc50e0d3b056ae6c91a453e9c7e54421dc13
Mohammad Toosi <m_dd@rogers.com>
2020-01-13 13:54:54 +0330

    fixed high bundle size in code splitting

--- webpacks/configGenerator.js
+++ webpacks/configGenerator.js
@@ -171,6 +171,7 @@ const configGenerator = target => {
           })
         ],
     module: {
+      exprContextCritical: false,
       strictExportPresence: true,
       rules: [
         {
@@ -276,38 +277,7 @@ const configGenerator = target => {
       ? {
           minimize: true,
           splitChunks: {
-            cacheGroups: {
-              default: false,
-              vendors: false,
-
-              // vendor chunk
-              vendor: {
-                // name of the chunk
-                name: 'vendor',
-
-                // async + async chunks
-                chunks: 'all',
-
-                // import file path containing node_modules
-                test: /node_modules/,
-
-                // priority
-                priority: 20
-              },
-              defaultVendors: {
-                reuseExistingChunk: true
-              },
-              // common chunk
-              common: {
-                minSize: 10000,
-
-                name: 'common',
-                minChunks: 2,
-                chunks: 'all',
-                priority: 10,
-                reuseExistingChunk: true
-              }
-            }
+            chunks: 'all'
           },
           minimizer: [
             new TerserPlugin(webpackUtils.terserPluginOptions),
